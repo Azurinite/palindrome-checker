20f458ed133b99bbf5e3ff708c9b0d325a12d3d2
Azurinite <ligmatitusB@gmail.com>
2026-02-06 14:20:17 -0800

    removed console.log

--- palindrome.js
+++ palindrome.js
@@ -7,7 +7,6 @@ function evaluatePalindrome(theGivenWord) {
     for (const character of realWord) {
         reversedWord = character + reversedWord;
     }
-    console.log(realWord, reversedWord);
 
     if (realWord === reversedWord) {
         return true;
