2181ed7d564d46b5429cc3ea7c558243d249d28e
Azurinite <ligmatitusB@gmail.com>
2026-02-06 15:33:55 -0800

    toLowerCase()

--- palindrome.js
+++ palindrome.js
@@ -2,7 +2,7 @@ const msg = prompt("GIVE ME A WORD!");
 evaluatePalindrome(msg)
 
 function evaluatePalindrome(theGivenWord) {
-    let realWord = theGivenWord.replace(" ", "");
+    let realWord = theGivenWord.replace(" ", "").toLowerCase();
     let reversedWord = "";
     for (const character of realWord) {
         reversedWord = character + reversedWord;
